f177d2cfea292aa955ba65a6983316847710f89c
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2016-09-18 07:31:10 +0800

    up

---
+++ SchoolTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using SchoolData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SchoolEntities context = new SchoolEntities();
+            var courses = from c in context.SD_Course select c;
+
+            foreach (SD_Course c in courses)
+            {
+                Console.WriteLine(c.Title);
+            }
+            Console.Read();
+        }
+    }
+}
